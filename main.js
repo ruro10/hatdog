edb96264ab85050e2fba3e94d030364fab0af104
ruro10 <tadaakisimp@gmail.com>
2024-09-28 23:44:53 +0800

    asd

---
+++ main.js
@@ -0,0 +1,68 @@
+const container = document.getElementById("container");
+const imageOne = document.querySelector(".image-1");
+const imageTwo = document.querySelector(".image-2");
+const btnYes = document.querySelector(".btn-yes");
+const btnNo = document.querySelector(".btn-no");
+
+function getRandomNumber(min, max) {
+  // Calculate the random number between min and max (inclusive)
+  const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+
+  return randomNumber;
+}
+
+btnNo.addEventListener("mouseover", (event) => {
+  const containerHeight = container.getBoundingClientRect().height;
+  const containerWidth = container.getBoundingClientRect().width;
+  const btnHeight = btnNo.getBoundingClientRect().height;
+  const btnWidth = btnNo.getBoundingClientRect().width;
+  const btnTop = btnNo.getBoundingClientRect().top;
+  const btnLeft = btnNo.getBoundingClientRect().left;
+
+  let newTop = btnTop;
+  let newLeft = btnLeft;
+  while (Math.abs(newTop - btnTop) < containerHeight / 3) {
+    newTop = getRandomNumber(0, containerHeight - btnHeight);
+  }
+
+  while (Math.abs(newLeft - btnLeft) < containerWidth / 3) {
+    newLeft = getRandomNumber(0, containerWidth - btnWidth);
+  }
+
+  btnNo.style.top = Math.floor(newTop) + "px";
+  btnNo.style.left = Math.floor(newLeft) + "px";
+
+  
+});
+
+btnYes.addEventListener("click", (e) => {
+  btnNo.classList.add("hide");
+  imageOne.classList.add("hide");
+  imageTwo.classList.remove("hide");
+});
+ 
+
+function generateMessage(noCount) {
+    btnNo.addEventListener("mouseover"), (evemt) =>{
+    
+        const messages = [
+      "No",
+      "Are you sure?",
+      "Pookie please",
+      "Don't do this to me :(",
+      "You're breaking my heart",
+      "I'm gonna cry...",
+    ];
+
+    const messageIndex = Math.min(noCount, messages.length - 1);
+    return messages[messageIndex];
+  }
+  
+  function changeImage(image) {
+    catImg.src = `img/cat-${image}.jpg`;
+  }
+  
+  function updateNoButtonText() {
+    noButton.innerHTML = generateMessage(noCount);
+  }
+}
